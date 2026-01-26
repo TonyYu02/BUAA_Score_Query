d17699e73892d3d270d6326209a0c2375ac31471
Tony Yu <tonynjupt@gmail.com>
2026-01-26 14:22:58 +0800

    Update cjcx_choose.py

--- cjcx_choose.py
+++ cjcx_choose.py
@@ -3,6 +3,7 @@ from cjcx import *
 if __name__ == "__main__":
     total_credits = sum(float(item['xf']) for item in grade_list)
     print(f"已修总学分：{total_credits}")
+    print(f"总绩点：{gpa}")
     print("-" * 30)
     semester_options = sorted(list({(item['year'], item['xq']) for item in grade_list}), reverse=True)
     for i, option in enumerate(semester_options, start=1):
@@ -23,7 +24,11 @@ if __name__ == "__main__":
     table_data = []
     for course in result:
         XFC = XFC + float(course['xf'])
-        table_data.append([course['kcmc'], course['xf'], course['kccj']])
-    head = ["课程名称", "学分", "成绩"]
+        if course['fslx'] == "百分制":
+            jd = 4 - 3 * (100 - float(course['kccj'])) ** 2 / 1600
+            table_data.append([course['kcmc'], course['xf'], course['kccj'], f"{jd:.2f}"])
+        else:
+            table_data.append([course['kcmc'], course['xf'], course['kccj']])
+    head = ["课程名称", "学分", "成绩", "绩点"]
     print(tabulate(table_data, headers=head, tablefmt="fancy_grid"))
     print(f"所选学期的总学分：{XFC}")
